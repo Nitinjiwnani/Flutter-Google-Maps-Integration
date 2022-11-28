97c9615f8e2f8e012c7d34fe043696a032a2af9c
Nitinjiwnani <jiwnani01@gmail.com>
2022-11-28 18:08:48 +0530

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static final CameraPosition _kGooglePlex = const CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(initialCameraPosition: _kGooglePlex),
+    );
+  }
+}
